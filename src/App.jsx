8ecc1c27154ea4dada44e8d11eeed47eb16f0426
Haointheh <haooo2351@gmail.com>
2026-06-01 14:30:16 +0545

    basic routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Sidebar from './components/Sidebar'
+import { Route , Routes} from 'react-router-dom'
+import Community from './pages/Community'
+import Credits from './pages/Credits'
+import Chatbox from './components/Chatbox'
 
 const App = () => {
   return (
-    <div className='text-2xl'>App</div>
+    <>
+    <div>
+      < div className='flex h-screen w-screen'>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<Chatbox />} />
+          <Route path='/credits' element={<Credits />} />
+          <Route path='/Community' element={<Community />} />
+        </Routes>
+      </div>
+    </div>
+  
+    </>
   )
 }
 
